c158a8d1792c4e2fb9639bac51fc62824cd2792f
Robin Bertram <robin.bertram@gmail.com>
2014-06-02 16:16:37 +0200

    Cleaned up.

--- app/routes.php
+++ app/routes.php
@@ -3,9 +3,9 @@ use Webbins\Routing\Router;
 
 Router::get('/', 'HomeController:index');
 
-Router::resource('stores', 'StoresController');
-
-// contains all available routes
+/**
+ * Contains all available routes.
+ */
 Router::get('routes', function() {
     return Webbins\Views\View::render('webbins/routes');
 });
