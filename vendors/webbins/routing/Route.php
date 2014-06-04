c450a475c39aacdba041420749f557c1d2d598a3
Robin Bertram <robin.bertram@gmail.com>
2014-06-04 17:03:03 +0200

    Made routing less URL sensitive.

--- vendors/webbins/routing/Route.php
+++ vendors/webbins/routing/Route.php
@@ -234,14 +234,16 @@ class Route {
      * @return  bool
      */
     public function matchPath($uri, $method) {
-        $path = str_replace('/', '\/', $this->getPrefix()).$this->getPattern();
+        $path = $this->getPrefix().$this->getPattern();
+
         // remove trailing slash
-        $path = rtrim($path, '\/');
+        $path = rtrim($path, '/');
+        $uri = rtrim($uri, '/');
 
         // if it's a match, then remove the first index (preg_match returns
         // the whole matching string as first index), set the parameters
         // and return true.
-        if (preg_match('/^'.$path.'$/', $uri, $matches) && $this->getMethod() == strtoupper($method)) {
+        if (preg_match('/^'.preg_quote($path).'$/i', $uri, $matches) && $this->getMethod() == strtoupper($method)) {
             array_shift($matches);
             $this->setParams($matches);
             return true;
