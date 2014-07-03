e59aff9cc6a542272c8cd199ab1098b8c8183c4d
Robin Bertram <robin.bertram@gmail.com>
2014-07-03 22:43:38 +0200

    Added some new parameters due to the new mail wrapper.

--- config.php
+++ config.php
@@ -26,41 +26,92 @@ $config = [
         'connect' => false,
 
         /**
-         * The host the application should connect to.
+         * Specify the host that the application should connect to.
          * @var  string
          */
-        'host'    => 'localhost',
+        'host'     => 'localhost',
 
         /**
          * Specify your username.
          * @var  string
          */
-        'user'    => '',
+        'username' => '',
 
         /**
          * Specify your password.
          * @var  string
          */
-        'pass'    => '',
+        'password' => '',
 
         /**
          * Specify your database name.
          * @var  string
          */
-        'db'      => '',
+        'db'       => '',
 
         /**
          * Specifies which database driver that should be used. You can find more
          * available drivers at http://www.php.net/manual/en/pdo.drivers.php.
          * @var  string
          */
-        'driver'  => 'mysql',
+        'driver'   => 'mysql',
 
         /**
          * Specifies which charset to use.
          * @var  string
          */
-        'charset' => 'utf8'
+        'charset'  => 'utf8'
+    ],
+
+    /**
+     * Mail uses the Swift Mailer library to send all mails. You can find more
+     * information about Swift Mailer at http://swiftmailer.org.
+     * @var  array
+     */
+    'mail' => [
+        /**
+         * Specify the host that the application should connect to.
+         * @var  string
+         */
+        'host' => '',
+
+        /**
+         * Specify the port that the application should use when connecting
+         * to the host.
+         * @var  int
+         */
+        'port' => 25,
+
+        /**
+         * This is he default mail address that all outgoing mails will be
+         * sent from. It's possible to override this value by passing a
+         * parameter to the mail class.
+         * @var  string
+         */
+        'sender'   => '',
+
+        /**
+         * This is supposed to be your name or your companys name. Like the
+         * default mail address, it's possible to override this value by
+         * passing a parameter to the mail class.
+         * @var  string
+         */
+        'alias'    => 'Robin Doe',
+
+        /**
+         * If you're using a email provider that need authentication you
+         * should specify your username here. It's possible to override
+         * this value by passing a parameter to the mail class.
+         * @var  string
+         */
+        'username' => '',
+
+        /**
+         * Specify the password of the username. The password may also be
+         * a API key.
+         * @var  string
+         */
+        'password' => '',
     ],
 
     /**
